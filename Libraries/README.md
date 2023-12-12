e12073f3e2cec9c5f81c060b60143552822d8a91
Sculas <contact@sculas.xyz>
2023-12-12 01:09:54 +0100

    fix: Fixed support for BCP 3.3.0+

--- Libraries/README.md
+++ Libraries/README.md
@@ -2,6 +2,6 @@
 
 For this mod to compile and function, [Brutal Company Plus][BCPlus] is required.
 You'll need to download it and place the DLL file in here.
-The name of the file should be `BrtualCompanyPlus.dll` (note the typo).
+The name of the file should be `BrutalCompanyPlus.dll`.
 
 [BCPlus]: https://thunderstore.io/c/lethal-company/p/Nips/Brutal_Company_Plus/
--- Patches/BCPatches.cs
+++ Patches/BCPatches.cs
@@ -5,23 +5,23 @@ using HarmonyLib;
 namespace BrutalCompanyAdditions.Patches;
 
 public static class BCPatches {
-    private static BCP.Data.EventEnum LastEvent = BCP.Data.EventEnum.None;
+    private static BrutalCompanyPlus.BCP.EventEnum LastEvent = BrutalCompanyPlus.BCP.EventEnum.None;
 
     [HarmonyPatch(typeof(BrutalCompanyPlus.Plugin), "SelectRandomEvent")]
     [HarmonyPrefix]
-    public static bool InjectCustomEvents(ref BCP.Data.EventEnum __result) {
+    public static bool InjectCustomEvents(ref BrutalCompanyPlus.BCP.EventEnum __result) {
         Plugin.Logger.LogWarning("Injecting custom events...");
 
         switch (EventRegistry.SelectableEvents.Count) {
             case 0:
-                __result = LastEvent = BCP.Data.EventEnum.None;
+                __result = LastEvent = BrutalCompanyPlus.BCP.EventEnum.None;
                 return false;
             case 1:
                 __result = LastEvent = EventRegistry.SelectableEvents[0];
                 return false;
         }
 
-        BCP.Data.EventEnum selectedEvent;
+        BrutalCompanyPlus.BCP.EventEnum selectedEvent;
         do {
             var eventId = UnityEngine.Random.Range(0, EventRegistry.SelectableEvents.Count);
             selectedEvent = EventRegistry.SelectableEvents[eventId];
@@ -40,11 +40,11 @@ public static class BCPatches {
 
     [HarmonyPatch(typeof(BrutalCompanyPlus.Plugin), "HandleEventSpecificAdjustments")]
     [HarmonyPrefix]
-    public static bool InjectEventHandlers(ref BCP.Data.EventEnum eventEnum, ref SelectableLevel newLevel) {
+    public static bool InjectEventHandlers(ref BrutalCompanyPlus.BCP.EventEnum eventEnum, ref SelectableLevel newLevel) {
         // Would preferably inject after this check, but we have no other choice than to do it here as well.
         if (newLevel.sceneName == "CompanyBuilding") {
             Plugin.Logger.LogWarning("Landed at The Company Building, skipping...");
-            eventEnum = BCP.Data.EventEnum.None;
+            eventEnum = BrutalCompanyPlus.BCP.EventEnum.None;
         }
 
         if (!EventRegistry.IsCustomEvent(eventEnum)) {
